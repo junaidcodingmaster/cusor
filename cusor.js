9f77d705c71cce4aae24cf466bca87fe9df8a201
Md.Junaid <72298298+junaidcodingmaster@users.noreply.github.com>
2021-04-25 15:59:50 +0530

    Add files via upload

---
+++ cusor.js
@@ -0,0 +1,25 @@
+class Cusor {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    
+    display(){
+      var pos =this.body.position;
+
+      push();
+
+  rectMode(CENTER);
+      rect(pos.x, pos.y, this.width, this.height);   
+      console.log("x : " + pos.x);
+      console.log("y : " + pos.y);
+      
+    pop();
+
+    }
+  };
